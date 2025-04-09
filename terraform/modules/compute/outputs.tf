67165b1d2f370d98f671475bc4e68b2c8bac9e4f
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-09 17:06:47 +0200

    outputs for jenkins ip and bastion ip

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,3 +1,11 @@
 output bastion_instance_id {
     value =  aws_instance.bastion.id
 }
+
+output "jenkins_private_ip" {
+  value = aws_instance.jenkins.private_ip
+}
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
