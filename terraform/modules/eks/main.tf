42816e43fc0dfa1d20467f34e90b2f7b6ec76508
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-21 20:31:16 +0200

    storage for nodes

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -59,7 +59,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn   = var.eks_node_role_arn
   subnet_ids      = var.subnet_ids
 
-  instance_types = [var.instance_type]
+  #instance_types = [var.instance_type]
 
 
   launch_template {
