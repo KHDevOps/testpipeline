32ee4698d43201ef9cdb143dab81d5520f1f4c1d
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-23 22:01:30 +0200

    argocd automated

--- terraform/environnements/dev/provider.tf
+++ terraform/environnements/dev/provider.tf
@@ -26,3 +26,39 @@ provider "aws" {
 
 provider "tls" {
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -8,9 +8,15 @@ terraform {
   }
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_secretsmanager_secret" "admin_ips" {
-  name        = "${var.cluster_name}-admin-ips"
+  name        = "${var.cluster_name}-admin-ips-${random_id.suffix.hex}"
   description = "Admin IP address allowed to access EKS cluster"
+  force_overwrite_replica_secret = true
+  recovery_window_in_days = 0
   
   tags = {
     Name        = "${var.cluster_name}-admin-ips"
