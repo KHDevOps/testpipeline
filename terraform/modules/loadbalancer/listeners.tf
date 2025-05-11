eb01216d116cb92acb0c1f15c6058247e0b89afd
Leomendoza13 <leo.mendoza@epita.fr>
2025-05-11 20:15:44 +0200

    feat(loadbalancer): added https lb listener

--- terraform/modules/loadbalancer/listeners.tf
+++ terraform/modules/loadbalancer/listeners.tf
@@ -13,6 +13,17 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.monitoring_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ingress_http_tg.arn
+  }
+}
 
 resource "aws_lb_listener_rule" "prometheus" {
   listener_arn = aws_lb_listener.http.arn
