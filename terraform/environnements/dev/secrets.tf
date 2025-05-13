697761c1946665c90f2b9d9d07e29f950a7b7bd3
Leomendoza13 <leo.mendoza@epita.fr>
2025-05-13 19:35:35 +0200

    feat(secrets): domain name secret added

--- terraform/environnements/dev/secrets.tf
+++ terraform/environnements/dev/secrets.tf
@@ -4,4 +4,5 @@ module "secrets" {
   cluster_name = local.cluster_name
   environment  = local.environment
   admin_ips    = var.admin_ips
+  domain_name  = var.domain_name
 }
--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -28,3 +28,20 @@ resource "aws_secretsmanager_secret_version" "admin_ips" {
   secret_id     = aws_secretsmanager_secret.admin_ips.id
   secret_string = jsonencode(var.admin_ips)
 }
+
+resource "aws_secretsmanager_secret" "domain_name" {
+  name        = "${var.cluster_name}-domain-name-${random_id.suffix.hex}"
+  description = "Domain name used for the platform"
+  force_overwrite_replica_secret = true
+  recovery_window_in_days = 0
+  
+  tags = {
+    Name        = "${var.cluster_name}-domain-name"
+    Environment = var.environment
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "domain_name" {
+  secret_id     = aws_secretsmanager_secret.domain_name.id
+  secret_string = var.domain_name
+}
